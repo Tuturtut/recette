9fd2747a741e0fdb1738417914442d5c184a267e
Arthur <arthursimonin88@gmail.com>
2025-02-21 22:11:05 +0100

    feat(style):Add new font for links

--- nav.php
+++ nav.php
@@ -13,9 +13,9 @@
 <body>
     <nav class="navigation">
         <ul>
-            <li class="link"><a href="index.php">Accueil</a><span>Accueil</span></li>
-            <li class="link"><a href="addReceipt.php">Ajouter une recette</a></li>
-            <li class="link"><a href="showDB.php">Voir la base de donnée</a></li>
+            <li class="link"><a href="index.php">Accueil</a></li>
+            <li class="link"><a href="addReceipt.php">Ajouter une <span class="toSerif">recette</span></a></li>
+            <li class="link"><a href="showDB.php">Voir la liste des <span class="toSerif">recette</span></a></li>
         </ul>
     </nav>
     
