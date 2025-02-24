48dfb373dc216fc0771a6a118539d26c22b66ad2
MhugninnES <emilien.schmitter1@gmail.com>
2025-02-24 13:57:44 +0100

    fix: suppressing style from merging

--- nav.php
+++ nav.php
@@ -7,7 +7,7 @@ session_start();?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="/recette/style.css">
+    <link rel="stylesheet" href="/recette/style/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Host+Grotesk:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
