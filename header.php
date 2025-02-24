46e4baa87e2b9d9a35fe9464e74887a290b27b01
Arthur <arthursimonin88@gmail.com>
2025-02-24 11:48:40 +0100

    refactor:Move style file

--- header.php
+++ header.php
@@ -5,14 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/recette/style/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Host+Grotesk:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
 </head>
 <body>
     <?php include 'nav.php'; ?>
-
-      <h1>Recipe.</h1>
-
-      <main>
+    
+    <main>
+        <h1 id=title>Recipe.</h1>
+        <div class="content">
