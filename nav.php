c32e6897a37ac38aab894998577642dcb4e2f57c
MhugninnES <emilien.schmitter1@gmail.com>
2025-02-24 13:26:32 +0100

    fix: fixing href in links

--- nav.php
+++ nav.php
@@ -14,11 +14,11 @@
 <body>
     <nav class="navigation">
         <ul>
-            <li class="link"><a href="/index.php">Accueil</a></li>
-            <li class="link"><a href="/addReceipt.php">Ajouter une recette</a></li>
-            <li class="link"><a href="/showDB.php">Voir la base de donnée</a></li>
-            <li class="link"><a href="/update.php">Changer la recette</a></li>
-            <li class="link"><a href="/sessions/registerForm.php">Inscription</a></li>
+            <li class="link"><a href="/recette/index.php">Accueil</a></li>
+            <li class="link"><a href="/recette/addReceipt.php">Ajouter une recette</a></li>
+            <li class="link"><a href="/recette/showDB.php">Voir la base de donnée</a></li>
+            <li class="link"><a href="/recette/update.php">Changer la recette</a></li>
+            <li class="link"><a href="/recette/sessions/registerForm.php">Inscription</a></li>
             <li class="link"><a href="/recette/sessions/loginForm.php">Login</a></li>
         </ul>
     </nav>
