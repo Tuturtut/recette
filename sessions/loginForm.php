aceea47c89e172b3434355d26260abe310c7495e
MhugninnES <emilien.schmitter1@gmail.com>
2025-02-24 13:42:22 +0100

    feat: adding loginForm

--- sessions/loginForm.php
+++ sessions/loginForm.php
@@ -4,3 +4,8 @@ include '../nav.php';
 
 ?>
 
+<form action="login.php" method="post">
+    <input type="text" name="username" placeholder="username">
+    <input type="password" name="password" placeholder="password">
+    <input type="submit" name="submit">
+</form>
